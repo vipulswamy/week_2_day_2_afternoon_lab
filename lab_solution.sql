b254b9a1803471022d5366ad32194b3b99b88da4
vipulswamy <mechswamy@gmail.com>
2022-10-12 17:17:34 +0200

    lab solution

---
+++ lab_solution.sql
@@ -0,0 +1,10 @@
+USE sakila;
+
+SELECT * from sakila.actor
+where first_name = "Joe";
+
+SELECT * from sakila.film
+where first_name = "Joe";
+
+SELECT * from sakila.customer
+where first_name = "Joe";
